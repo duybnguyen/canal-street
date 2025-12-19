7ef21bcad9386b997def0d048e0b2332824b0f71
duy nguyen <duybnguyen04@gmail.com>
2025-12-19 01:24:53 -0500

    Fix Bug #1:  - Corrected SQL statement to use min and max price in its query  - Corrected parameter binding

--- src/main/java/org/yearup/data/mysql/MySqlProductDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProductDao.java
@@ -23,10 +23,14 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
     {
         List<Product> products = new ArrayList<>();
 
-        String sql = "SELECT * FROM products " +
-                "WHERE (category_id = ? OR ? = -1) " +
-                "   AND (price <= ? OR ? = -1) " +
-                "   AND (subcategory = ? OR ? = '') ";
+        String sql = """
+                SELECT *
+                FROM products
+                WHERE (category_id = ? OR ? = -1)
+                  AND (price >= ? OR ? = -1)
+                  AND (price <= ? OR ? = -1)
+                  AND (subcategory = ? OR ? = '')
+                """;
 
         categoryId = categoryId == null ? -1 : categoryId;
         minPrice = minPrice == null ? new BigDecimal("-1") : minPrice;
@@ -38,10 +42,18 @@ public class MySqlProductDao extends MySqlDaoBase implements ProductDao
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, categoryId);
             statement.setInt(2, categoryId);
+
             statement.setBigDecimal(3, minPrice);
             statement.setBigDecimal(4, minPrice);
-            statement.setString(5, subCategory);
-            statement.setString(6, subCategory);
+
+            statement.setBigDecimal(5, maxPrice);
+            statement.setBigDecimal(6, maxPrice);
+
+            statement.setString(7, subCategory);
+            statement.setString(8, subCategory);
+
+
+
 
             ResultSet row = statement.executeQuery();
 
