1f331f28a636e075cd210953463cf973dc0e7644
duy nguyen <duybnguyen04@gmail.com>
2025-12-19 01:57:42 -0500

    Add all necessary methods in ShoppingCartDao interface

--- src/main/java/org/yearup/data/ShoppingCartDao.java
+++ src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -1,9 +1,13 @@
 package org.yearup.data;
 
+import org.yearup.models.Product;
 import org.yearup.models.ShoppingCart;
 
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
-    // add additional method signatures here
+    void addToCart(int userId, int productId);
+    void updateQuantity(int userId, int productId, int quantity);
+    void clearCart(int userId);
 }
+
